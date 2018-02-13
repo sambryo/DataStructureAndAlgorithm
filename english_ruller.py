95c399205302ecd3e1d7e85e7150ce7f13dbaa36
Samuel <terefesam@gmail.com>
2018-02-13 10:35:10 -0800

    insertion_Sort

--- english_ruller.py
+++ english_ruller.py
@@ -7,7 +7,7 @@ def draw_line(tick_length, tick_label=''):
 
 def draw_interval(center_length):
     if center_length > 0:
-        draw_interval(center_length - 1)
+        draw_interval(center_length - 1
         draw_line(center_length)
         draw_interval(center_length - 1)
 
